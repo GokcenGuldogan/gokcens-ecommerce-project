94ac7ff8c0d06cf4abbf85fcb420b511de5e3a1a
GokcenGuldogan <166025609+GokcenGuldogan@users.noreply.github.com>
2025-06-03 23:04:11 +0300

    tailwind yuklendi

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Smile } from "lucide-react";
 
 
 
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Tailwind Çalışıyor!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind çalışıyor!
+      </h1>
+      <Smile className="text-green-600 w-8 h-8 mt-4" />
+    </>
   );
 }
+
