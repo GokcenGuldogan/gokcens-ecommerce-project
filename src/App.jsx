ebe28aa7345a53658e6918b5d303e8b00774a196
GokcenGuldogan <166025609+GokcenGuldogan@users.noreply.github.com>
2025-06-24 23:00:10 +0300

    son commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import HomePage from "./pages/HomePage";
 
+
+
+
+
 export default function App() {
   return (
     <div>
